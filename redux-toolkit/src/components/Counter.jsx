5092aef2ea27e349df29133bbf4024ae984ddc80
Shape Lee <shape1026@gmail.com>
2023-03-23 16:45:57 +0900

    Study: Add Redux-Toolkit createAsyncThunk

--- redux-toolkit/src/components/Counter.jsx
+++ redux-toolkit/src/components/Counter.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import counterSlice, { add } from '../module/counterSlice';
+import counterSlice, { add, asyncUpfetch } from '../module/counterSlice';
 
 
 const counter = () => {
   const count = useSelector((state) => state.counter.number)
+  const status = useSelector((state) => state.counter.status)
   const dispatch = useDispatch()
   
   return (
     <div>
-      <h1>{count}</h1>
+      <h1>{count} | {status}</h1>
       <button onClick={() => {
         // dispatch({ type: 'counterSlice/add', step: 2 })
         dispatch(add(2))
       }}>+</button>
+      <button onClick={() => {
+        dispatch(asyncUpfetch())
+      }}>+ async</button>
     </div>
   );
 };
--- redux-toolkit/src/module/counterSlice.js
+++ redux-toolkit/src/module/counterSlice.js
@@ -1,8 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
   number: 0,
+  status: 'welcome',
 }
+
+export const asyncUpfetch = createAsyncThunk('counterSlice/asyncUpfetch',
+  async () => {
+    const resp = await fetch('https://api.countapi.xyz/hit/opesaljkdfsadf.com/visits')
+    const data = await resp.json()
+    return data.value
+  }
+)
 
 export const counterSlice = createSlice({
   name: 'counter',
@@ -12,7 +21,20 @@ export const counterSlice = createSlice({
       console.log(action)
       state.number = state.number + action.payload
     }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(asyncUpfetch.pending, (state, action) => {
+      state.status = 'Loding'
+    })
+    builder.addCase(asyncUpfetch.fulfilled, (state, action) => {
+      state.number = action.payload
+      state.status = 'complete'
+    })
+    builder.addCase(asyncUpfetch.rejected, (state, action) => {
+      state.status = 'fail'
+    })
   }
+  
 })
 
 export const { add } = counterSlice.actions
